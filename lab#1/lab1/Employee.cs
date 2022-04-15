3c6af8325f6d2548f656402b44bad3f657415ae3
Vladimir0770 <89249102+Vladimir0770@users.noreply.github.com>
2022-04-15 14:24:10 +0300

    Add files via upload
    Laboratory work#1

---
+++ lab#1/lab1/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace lab1
+{
+    class Employee
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int moneyPerDay { get; set; }
+        public int amountOfWorkedDays { get; set; }
+
+        public int getSalary() { return moneyPerDay * amountOfWorkedDays; }
+
+        public string getFullName() { return name + " " + surname;  }
+    }
+}
