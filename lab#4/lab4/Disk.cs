757bb1330fb519e863cdcb41537a2c4e513c8335
Vladimir0770 <89249102+Vladimir0770@users.noreply.github.com>
2022-06-11 18:25:33 +0300

    Update Disk.cs

--- lab#4/lab4/Disk.cs
+++ lab#4/lab4/Disk.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel;
@@ -7,7 +7,7 @@ namespace lab4
 {
     public abstract class Disk
     {
-        private int size;
+        private int size { get; set; }
         protected string type;
 
         protected Disk(int size)
@@ -16,6 +16,12 @@ namespace lab4
         }
 
         public int getSize() { return size; }
+
+        public void setSize(int size)
+        {
+            if (size > 0)
+                this.size = size;
+        }
 
         public string getType() { return type; }
 
