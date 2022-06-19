0ffc83ce03afb01ec47cd792b3716ad687c50f66
Vladimir0770 <89249102+Vladimir0770@users.noreply.github.com>
2022-06-19 13:52:20 +0300

    Переделал все

---
+++ lab#3/lab3.2/lab3.2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace lab3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> numbers = new Dictionary<string, int>()
+            {
+                { "c", 56},
+                { "g", 24},
+                { "a", 47},
+                { "e", 1},
+                { "y", 44},
+                { "z", 9},
+                { "b", 27},
+                { "d", 55},
+                { "x", 97},
+                { "f", -12}
+            };
+
+            Console.WriteLine("Our dictionaty before operatoins");
+            printDictionary(numbers);
+
+            deleteMinAndMaxNumber(numbers);
+            numbers = sortDictionaryByKey(numbers);
+
+            Console.WriteLine("Our dictionaty after operatoins");
+            printDictionary(numbers);
+
+            string filePath = "sortedDictionary";
+
+            jsonSerialize(numbers, filePath);
+        }
+
+        private static void deleteMinAndMaxNumber(Dictionary<string, int> numbers)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            string keyOfMinNum = "", keyOfMaxNum = "";
+
+            foreach (var item in numbers)
+            {
+                if (item.Value < min)
+                {
+                    min = item.Value;
+                    keyOfMinNum = item.Key;
+                }
+                else if (item.Value > max)
+                {
+                    max = item.Value;
+                    keyOfMaxNum = item.Key;
+                }
+            }
+
+            Console.WriteLine("The biggest number is " + max);
+            Console.WriteLine("The least number is " + min);
+
+            numbers.Remove(keyOfMinNum);
+            numbers.Remove(keyOfMaxNum);
+        }
+
+        private static void printDictionary(Dictionary<string, int> numbers)
+        {
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item.Key + " -> " + item.Value);
+            }
+        }
+
+        private static Dictionary<string, int> sortDictionaryByKey(Dictionary<string, int> numbers)
+        {
+            return numbers.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        private static void jsonSerialize(object data, string filePath)
+        {
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            if (File.Exists(filePath)) File.Delete(filePath);
+
+            StreamWriter sw = new StreamWriter(filePath);
+            JsonWriter jsonWriter = new JsonTextWriter(sw);
+
+            jsonSerializer.Serialize(jsonWriter, data);
+
+            jsonWriter.Close();
+            sw.Close();
+        }
+    }
+}
---
+++ lab#3/lab3.3/lab3.3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char C = 's';
+            string str1 = "mercedes apple BMW Alibaba Amazon"; // consisits one
+            string str2 = "mercedes apple BMW Alibaba Airpods"; // consisits two
+            string str3 = "apple BMW Alibaba"; /// consisits none
+
+            string[] arr = str3.Split(" ");
+            List<string> list = new List<string>(arr);
+
+            Console.WriteLine("Our string -> " + str1);
+
+            int counter = 0;
+            string element = "";
+
+            counter = list.Where(item => item.Last() == C).Count();
+
+            if (counter == 1)
+            {
+                Console.WriteLine(element);
+            }
+            else if (counter == 0)
+            {
+                Console.WriteLine("*Empty line, as there is no words ending with \"" + C + "\"*");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
